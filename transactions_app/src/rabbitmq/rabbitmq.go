ac12cbcace31f2e985af8f0475e5dc3ac29e8dd4
Grant Komendantyan <grant.komendantian@gmail.com>
2023-02-12 00:40:53 +0300

    Now consuming messages in rabbitmq.go; there is a SendMessages function.

--- transactions_app/src/rabbitmq/rabbitmq.go
+++ transactions_app/src/rabbitmq/rabbitmq.go
@@ -2,8 +2,12 @@ package rabbitmq
 
 import (
 	"log"
+	"context"
+	"time"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
+
+var CH *amqp.Channel
 
 func failOnError(err error, msg string) {
 	if err != nil {
@@ -11,12 +15,74 @@ func failOnError(err error, msg string) {
 	}
 }
 
-func GetChannel() *amqp.Channel {
+func GetChannel(){
 	conn, err := amqp.Dial("amqp://rabbitmq:rabbitmq@rabbitmq_app:5672/")
 	failOnError(err, "Failed to connect to RabbitMQ")
 
 	ch, err := conn.Channel()
 	failOnError(err, "Failed to open a channel")
 
-	return ch
+	CH = ch
+}
+
+func ConsumeMessages() {
+	ch := CH
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+}
+
+func SendMessages() {
+    ch := CH
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	body := "Hello World!"
+	err = ch.PublishWithContext(ctx,
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
+	log.Printf(" [x] Sent %s\n", body)
 }
