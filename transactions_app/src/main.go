8be83f714a70b5302a3480efaa1e72bc4f5a8f5b
Grant Komendantyan <grant.komendantian@gmail.com>
2023-02-11 23:44:02 +0300

    Receiving messages in main.go.

--- transactions_app/src/main.go
+++ transactions_app/src/main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"time"
+	"fmt"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"log"
 )
 
 import (
@@ -9,7 +13,15 @@ import (
 	"transactionSystem/controllers"
 )
 
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
 func main() {
+
+    time.Sleep(time.Second * 5)
 
 	models.ConnectDatabase()
 
@@ -21,6 +33,57 @@ func main() {
 	router.PATCH("/update_user", controllers.UpdateUser)
 	router.DELETE("/delete_user", controllers.DeleteUser)
 	router.POST("/withdraw_balance", controllers.WithdrawBalance)
+
+	ticker := time.NewTicker(time.Second*5)
+
+	fmt.Println("TestTestTestTestTestTestTestTestTestTestTestTestTestTest!!!")
+
+	go func() {
+	    for {
+	        select {
+	        case t:= <- ticker.C:
+	            fmt.Println("Ticker ticked!", t)
+	        }
+	    }
+	}()
+
+	conn, err := amqp.Dial("amqp://rabbitmq:rabbitmq@rabbitmq_app:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+
 
 	router.Run("0.0.0.0:8080")
 
